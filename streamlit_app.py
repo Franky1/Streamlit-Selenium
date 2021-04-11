8600ca33a9403d1fdfdaf8dc89355f3d0cd11d0f
Franky1 <arvid.teichtmann@gmx.de>
2021-04-11 18:16:37 +0200

    tried options

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-features=NetworkService")
